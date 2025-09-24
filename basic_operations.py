a5e4b85179770d4bcfc95cd008ee0a5ba3140f0a
Zeyad Amin <zeyadamin291@gmail.com>
2025-09-24 20:47:32 +0300

    second commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+diff_result = number1 - number2
+prod_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {diff_result}")
+print(f"Multiplication of {number1} and {number2} is {prod_result}")
