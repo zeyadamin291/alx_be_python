3fdba2d753a8cf203667a2d8cbe3ea7870ed3e21
Zeyad Amin <zeyadamin291@gmail.com>
2025-09-24 21:35:55 +0300

    task 2

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# multiplication_table.py
+
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
