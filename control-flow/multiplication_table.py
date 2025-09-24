03c1d2d3aa0c321b3e42ed542ffe1df973b1bb04
Zeyad Amin <zeyadamin291@gmail.com>
2025-09-24 21:37:05 +0300

    task 2

--- control-flow/multiplication_table.py
+++
@@ -1,7 +0,0 @@
-# multiplication_table.py
-
-number = int(input("Enter a number to see its multiplication table: "))
-
-for i in range(1, 11):
-    product = number * i
-    print(f"{number} * {i} = {product}")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+# pattern_drawing.py
+
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
