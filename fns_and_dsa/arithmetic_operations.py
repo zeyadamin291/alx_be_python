3f8087b5c6db2f62f210268376de8e15c77145d0
Zeyad Amin <zeyadamin291@gmail.com>
2025-09-25 12:08:50 +0300

    task 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,17 @@
 def perform_operation(num1, num2, operation):
     result =0
+    num1 = float(num1)
+    num2 = float(num2)
     if operation == 'add':
-        result = float(num1) + float(num2)
+        result = num1 + num2
     elif operation == 'subtract':
-        result= float(num1) - float(num2)
+        result= num1 - num2
     elif operation == 'multiply':
-        result= float(num1) * float(num2)
+        result= num1 * num2 
     elif operation == 'divide':
-        if float(num2) == 0:
+        if num2 == 0:
             return "Error: Division by zero"
-        result = float(num1) / float(num2)
+        result = num1 / num2
     else:
         return "Error: Invalid operation"
     return result
