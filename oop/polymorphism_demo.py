5893e82ffccc855f814129039665507c461ef79a
Zeyad Amin <zeyadamin291@gmail.com>
2025-11-01 17:53:31 +0200

    Finished person 1 commands

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -6,23 +6,19 @@ class Shape:
     
 
 class Rectangle(Shape):
-    def __init__(self , length , width):
+    def __init__(self, length, width):
         super().__init__()
         self.length = length
         self.width = width
 
     def area(self):
-        return self.length*self.width
+        return self.length * self.width
     
 
 class Circle(Shape):
-    def __init__(self , radius):
+    def __init__(self, radius):
         super().__init__()
         self.radius = radius
+        
     def area(self):
-        return math.pi* self.radius**2
-
-
-
-
-
+        return math.pi * (self.radius ** 2)
