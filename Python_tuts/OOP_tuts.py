6347a8ec4f57d71ae8a6ca5d15670de55190fa45
Zeyad Amin <zeyadamin291@gmail.com>
2025-11-01 16:12:11 +0200

    Finished person 1 commands

--- Python_tuts/OOP_tuts.py
+++ Python_tuts/OOP_tuts.py
@@ -1,14 +1,2 @@
-# # Exercise 1: Class Methods for Counting Instances Instruction:
-
-# Create a class Book with a class variable total_books to count the number of book instances created.
-# Implement a class method display_total_books() to display the total number of books created.
-
-
-class Book:
-    total_books = 0
-    def __init__(self):
-        total_books+=1
-        
-    @classmethod
-    def display_total_books(cls):
-        print(cls.total_books)
+name = input()
+print("Hello,", name)
--- oop/library_system.py
+++ oop/library_system.py
@@ -1,7 +1,10 @@
 class Book:
-    def __init__(self , title , author):
-        self.title= title
+    def __init__(self, title, author):
+        self.title = title
         self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
 
 
 class EBook(Book):
@@ -9,11 +12,18 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"{self.title} by {self.author}, file size: {self.file_size}KB"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, page count: {self.page_count}"
+
 
 class Library:
     def __init__(self):
@@ -24,25 +34,22 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(f"{book.title} by {book.author}, page count: {book.page_count}")
+            print(book)
 
 
 def main():
-    # Create a Library instance
     my_library = Library()
 
-    # Create instances of each type of book
     classic_book = Book("Pride and Prejudice", "Jane Austen")
     digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-    # Add books to the library
     my_library.add_book(classic_book)
     my_library.add_book(digital_novel)
     my_library.add_book(paper_novel)
 
-    # List all books in the library
     my_library.list_books()
+
 
 if __name__ == "__main__":
     main()
